a2341501a0ea5561cefb497aab24a59327fb16dd
Erika Ding <erikajding@gmail.com>
2025-04-14 13:59:25 -0400

    Fixed minor merge error

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -43,10 +43,7 @@ def create_app():
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
-    app.logger.info('current_app(): registering blueprints with Flask app object.')   
-    app.register_blueprint(simple_routes)
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.logger.info('current_app(): registering blueprints with Flask app object.')
     # Blueprint for SMEs
     app.register_blueprint(posts,       url_prefix='/posts')
     app.register_blueprint(sme,       url_prefix='/sme')
