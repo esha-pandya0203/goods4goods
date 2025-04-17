b8677c728a2402854219c9c5b38d1e3178f1baab
Erika Ding <erikajding@gmail.com>
2025-04-17 03:30:15 -0400

    Deleted unused database-files sql files

--- database-files/09_sme-data.sql
+++
@@ -1,9 +0,0 @@
-USE `goods4goods`;
-
-# ---------------------------------------------------------------------- #
-# Insert into "SME"                                                 #
-# ---------------------------------------------------------------------- #
-INSERT INTO SME (firstName,lastName,email, street, city, state, zipcode) VALUES
-('Daniel', 'Kimmel', 'dkimm@gmail.com','Spruce Street', 'Boston','Massachusetts', '02115'),
-('Scarlett', 'Cole', 'scole@gmail.com','Hillside Street', 'Boston', 'Massachusetts', '02120'),
-('Jim', 'Scorn', 'jscorn@gmail.com','Maple Avenue','Bedford', 'New York', '10506');
--- database-files/14_favitems-data.sql
+++
@@ -1,6 +0,0 @@
-USE `goods4goods`;
-
-INSERT INTO FavoriteItems (item_id, sme_id) VALUES
-(1, 1),
-(2, 2),
-(3, 3);
