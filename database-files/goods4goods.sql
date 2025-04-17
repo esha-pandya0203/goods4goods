df502d9476ef1d4d0af172c2bf9245c1ec8e69a9
narang-an <narang.an@northeastern.edu>
2025-04-17 09:40:26 -0400

    deleted unused files

--- database-files/goods4goods.sql
+++ database-files/goods4goods.sql
@@ -27,7 +27,6 @@ INSERT INTO Admin (first_name, last_name) VALUES
 ('Bob', 'Williams'),
 ('Charlie', 'Davis');
 
-
 insert into Admin (first_name, last_name) values ('Mag', 'Pettyfar');
 insert into Admin (first_name, last_name) values ('Lombard', 'Landsman');
 insert into Admin (first_name, last_name) values ('Stacie', 'Gibbard');
@@ -322,7 +321,7 @@ INSERT INTO Item (posted_by, description, product_name, image_url, target_price)
 (2, 'Gently used sofa set, comfortable.', 'Sofa Set', 'https://i.pinimg.com/736x/18/08/dc/1808dcedf3ad854977805c15d45d557c.jpg', 200),
 (3, 'Vintage desk lamp, perfect for home office.', 'Desk Lamp', 'https://i.pinimg.com/736x/a1/2d/85/a12d8585003e9a171698980a0e859bb0.jpg', 30),
 (2, 'Modern recliner in excellent condition.', 'Recliner Chair', 'https://i.pinimg.com/474x/4b/d4/09/4bd4094db0140173aa820bcb242888d8.jpg', 180),
-(2, 'Stylish round dining table, slightly used.', 'Dining Table', 'https://i.pinimg.com/474x/0c/44/1c/0c441c0084f0fc0d2fe6cba8a53cd98c.jpg', 120),
+(29, 'Stylish round dining table, slightly used.', 'Dining Table', 'https://i.pinimg.com/474x/0c/44/1c/0c441c0084f0fc0d2fe6cba8a53cd98c.jpg', 120),
 (29, 'Single bed frame, lightly used with storage.', 'Bed Frame', 'https://i.pinimg.com/474x/80/64/79/806479b6c6ffb97c70590758967db3c9.jpg', 75),
 
 (3, 'Compact microwave, works perfectly.', 'Microwave', 'https://i.pinimg.com/474x/c7/13/3d/c7133d0600780562444e21e03e8abce7.jpg', 50),
